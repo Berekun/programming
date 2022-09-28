8876203dfcd554ebe902e63ba2ee766cf23e39fa
Berekun <xnatsuhd@gmail.com>
2022-09-28 16:46:20 +0200

    ejercicio num prim

---
+++ Exercises/Exercises/Exercises.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Exercises
+    {
+        public static bool GetEveOrOdd(double a)
+        {
+            return ((a % 2) == 0);
+        }
+
+        public static bool IsPrime(int number)
+        {
+            double solution = 0;
+
+            for (int i = 2; i < number; i++)
+            {
+                solution += number % i;
+
+                if (solution == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        public static double GetCircleArea(double radio)
+        {
+            double pi = Math.PI;
+
+            return pi * (radio * radio);
+        }
+
+        public static double GetRectangleArea(double height, double weight)
+        {
+            return weight * height;
+        }
+
+        public static double GetDistance2points(double x1,double x2, double y1, double y2)
+        {
+
+            return 
+
+
+        }
+
+    }
+}
---
+++ Exercises/Exercises/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 29;
+            bool solution;
+
+            solution = Exercises.IsPrime(number);
+            Console.WriteLine(solution);
+
+            Console.WriteLine("---------------");
+
+            {
+                double radio, x;
+                radio = 8;
+
+                x = Exercises.GetCircleArea(radio);
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("---------------");
+
+            {
+                double height, weight, x;
+                height = 7.2;
+                weight = 5.3;
+
+                x = Exercises.GetRectangleArea(height,weight);
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
