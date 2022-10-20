6bf483c8906df70c071010341e6fff2810fbf773
Berekun <xnatsuhd@gmail.com>
2022-10-20 19:11:06 +0200

    20/10/2022

--- ImageFilters/ImageFilters/Paintools.cs
+++ ImageFilters/ImageFilters/Paintools.cs
@@ -2,14 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace ImageFilters
 {
     internal class Paintools
     {
-        public static DAM.Image FillRectangle(DAM.Image image,int x, int y,int width, int height,RGBA color)
+        public static void FillRectangle(Image image,int x, int y,int width, int height,RGBA color)
         {
             for(int h=y;h <= height;h++)
             {
@@ -18,8 +20,44 @@ namespace ImageFilters
                     image.SetPixel(h,w,color);
                 }
             }
-
-            return image;
         }
+
+        public static void Changecolor(Image image)
+        {
+            for(int h= 0; h <= image.Height;h++)
+            {
+                for (int w= 0;w <=image.Width;w++)
+                {
+                    RGBA value = image.GetPixelAt(w,h);
+                    double values = (value.r + value.g + value.b) /3;
+                    value.r = values;
+                    value.g = values;
+                    value.b = values;
+                    value.a = 1.0;
+
+                    //RGBA grey = new RGBA(values, values, values, 1.0);
+
+                    image.SetPixel(w,h,value);
+                }
+            }
+        }
+
+        public static void MultiplyPixel(Image image,double r,double g,double b,double a)
+        {
+            for (int h = 0; h <= image.Height; h++)
+            {
+                for (int w = 0; w <= image.Width; w++)
+                {
+                    RGBA value = image.GetPixelAt(w, h);
+                    value.r = value.r* r;
+                    value.g = value.g* g;
+                    value.b = value.b* b;
+                    value.a = a;    
+                    image.SetPixel(w, h, value);
+                }
+            }
+        }
+
+
     }
 }
--- ImageFilters/ImageFilters/Program.cs
+++ ImageFilters/ImageFilters/Program.cs
@@ -4,12 +4,13 @@ namespace ImageFilters
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        static void Test1()
         {
             {
                 string path = "C:\\Users\\danberinf\\Desktop\\Images\\test.png";
-                DAM.Image img=new DAM.Image();
-                img.Config(800,600,new DAM.RGBA(1.0,0.0,0.0,1.0));
+                Image img = new Image();
+                img.Config(800, 600, new DAM.RGBA(1.0, 0.0, 0.0, 1.0));
                 img.Save(path);
             }
 
@@ -19,13 +20,42 @@ namespace ImageFilters
                 y = 20;
                 heigth = 500;
                 width = 100;
-                string path = "C:\\Users\\danberinf\\Desktop\\Images\\test.png";
-                DAM.RGBA color = new RGBA(4.0, 6.0, 8.0, 1.0);
-                DAM.Image img2 = new DAM.Image();
-                img2.Config(800, 600, new DAM.RGBA(1.0, 0.0, 0.0, 1.0));
-                img2 = Paintools.FillRectangle(img2, x, y, width, heigth, color);
+                string path = "C:\\Users\\danberinf\\Desktop\\Images\\test1.png";
+                RGBA color = new RGBA(4.0, 6.0, 8.0, 1.0);
+                Image img2 = new Image();
+                img2.Config(800, 600, new RGBA(1.0, 0.0, 0.0, 1.0));
+                Paintools.FillRectangle(img2, x, y, width, heigth, color);
+                Paintools.FillRectangle(img2, x, y, width, heigth, color);
                 img2.Save(path);
             }
+        }
+
+        static void Test2()
+        {
+            string path = "C:\\Users\\danberinf\\Desktop\\Images\\";
+            string in_path = path + "foto_asta.jpg";
+            string out_path = path + "bw.jpg";
+            Image img = new Image();
+            img.Load(in_path);
+            Paintools.Changecolor(img);
+            img.Save(out_path);
+        }
+
+        static void Test3()
+        {
+            string path = "C:\\Users\\danberinf\\Desktop\\Images\\";
+            string in_path = path + "foto_asta.jpg";
+            string out_path = path + "multiply.jpg";
+            Image img = new Image();
+            img.Load(in_path);
+            Paintools.MultiplyPixel(img,0,0,1,1);
+            img.Save(out_path);
+        }
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
         }
     }
 }
