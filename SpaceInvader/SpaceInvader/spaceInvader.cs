048f1ae964c13d4c81d3742a16428d58b265ef2a
Berekun <xnatsuhd@gmail.com>
2022-11-17 17:16:52 +0100

    17/11/22

--- SpaceInvader/SpaceInvader/spaceInvader.cs
+++ SpaceInvader/SpaceInvader/spaceInvader.cs
@@ -9,8 +9,11 @@ namespace SpaceInvader
 {
     internal class SpaceInvader : DAM.IGameDelegate
     {
+        world world;
         public void OnDraw(IAssetManager manager, IWindow window, ICanvas canvas)
         {
+            canvas.Clear(1.0f, 1.0f, 1.0f, 1.0f);
+            canvas.SetCamera(world.x, world.y, world.x + world.width, world.y + world.height, true);
             
         }
 
@@ -21,7 +24,15 @@ namespace SpaceInvader
 
         public void OnLoad(IAssetManager manager, IWindow window)
         {
-           
+            //Caracteristicas del mundo
+            world = new world();
+            world.Image = manager.LoadImage("resource\\fondo.png");
+            float arimage = world.Image.Width / world.Image.Height;
+            world.height = 20.0f;
+            world.width = world.height / arimage;
+            world.y = -10.0f;
+            world.x = -7.5f;
+            world.a = 1.0f;
         }
 
         public void OnUnload(IAssetManager manager, IWindow window)
