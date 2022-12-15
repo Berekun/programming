beb93fa3f5d90f0918924941723f50f34896f2d6
Berekun <xnatsuhd@gmail.com>
2022-12-15 18:22:47 +0100

    15/12/22

---
+++ ChessTest/ChessLib/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLib
+{
+    public enum FigureType
+    {
+        BISHOP, KING, QUEEN, PAWN, ROCK, KNIGHT
+    }
+
+    public enum FigureColor
+    {
+        BLACK, WHITE
+    }
+    internal class Figure
+    {
+        private int _x, _y;
+        private FigureColor _color;
+        private FigureType _type;
+    } 
+
+
+}
