d74ab1cf26eba5fbf1f8c29fd69c87a1cf8678d9
Berekun <xnatsuhd@gmail.com>
2023-02-16 18:59:18 +0100

    19/02/23

--- Tree/Tree/Program.cs
+++ Tree/Tree/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,80 +8,97 @@ namespace Tree
 {
     internal class TreeNode<T>
     {
-        //Attrib
-        private WeakReference<TreeNode<T>>? _parent;
         private List<TreeNode<T>> _children = new List<TreeNode<T>>();
+        WeakReference<TreeNode<T>>? _parent;
+        private T item;
 
-        public T item;
-
-        //Properties
+        public delegate int Comparer(T a, T b);
+        public delegate TreeNode<T> Visitor(TreeNode<T> node);
         public int ChildCount => _children.Count;
 
-        public TreeNode<T>? parent => GetParent();
 
-        //Construc
-        public TreeNode()
-        {
-        }
         public TreeNode(T item)
         {
             this.item = item;
         }
 
-        public TreeNode<T>? GetParent()
+        public void AddChlidren(TreeNode<T> child)
+        {
+            if (child == null) return;
+
+            child._parent = new WeakReference<TreeNode<T>>(this);
+            _children.Add(child);
+
+        }
+        TreeNode<T>? GetParent()
         {
             if (_parent == null) return null;
             TreeNode<T>? result = null;
             _parent.TryGetTarget(out result);
             return result;
         }
-
-        public void AddChild(TreeNode<T>? child)
+        public void UnLink()
         {
-            if (child == null) return;
-
-            child._parent = new WeakReference<TreeNode<T>>(this);
-            _children.Add(child);
-        }
-
-        public void SetParent(TreeNode<T>? newParent)
-        {
-            if (newParent == null) return;
-            Unlink();
-            newParent.AddChild(this);
-        }
-
-        public int GetIndexOf(TreeNode<T>? value)
-        {
-            if(value == null) return -1;
-
-            for (int i = 0; i < ChildCount; i++)
-            {
-                if (_children[i] == value)
-                    return i;
-            }
-            return -1;
-        }
-
-        public void Unlink()
-        {
-            TreeNode<T>? p = GetParent();
+            var p = GetParent();
             if (p == null) return;
 
-            int index = p.GetIndexOf(this);
-            p._children.RemoveAt(index);
+            int index = p.GetChildIndex(this);
+            p?._children.RemoveAt(index);
             p = null;
+        }
+
+        public void SetParent(TreeNode<T>? parent)
+        {
+            if (parent != null)
+            {
+                UnLink();
+                parent.AddChlidren(this);
+            }
+        }
+        public int GetChildIndex(TreeNode<T> node)
+        {
+            for (int i = 0; i < ChildCount; i++)
+            {
+                if (node == _children[i]) return i;
+            }
+
+            return -1;
         }
 
         public TreeNode<T> GetRoot()
         {
-            var p = GetParent();
-            if (p == null) return this;
-            return p.GetRoot();
+            var par = GetParent();
+            if (par == null) return this;
+            return par.GetRoot();
         }
 
+        public bool Contains(T value, Comparer comp)
+        {
+            return FindNodeWithItem(value, comp) != null;
+        }
 
+        public TreeNode<T>? FindNodeWithItem(T value, Comparer compa)
+        {
+            if (value == null) return null;
+            if (compa(value, this.item) == 1)
+                return this;
+            foreach (var child in _children)
+            {
+                var itemm = child.FindNodeWithItem(value, compa);
+                if (itemm != null)
+                    return itemm;
+            }
 
+            return null;
+        }
 
+        public void Visit(Visitor visitor)
+        {
+            visitor(this);
+            foreach (var child in _children)
+            {
+                child.Visit(visitor);
+            }
+        }
     }
 }
---
+++ Tree/Tree/Set.cs
@@ -0,0 +1,66 @@
+﻿namespace Tree
+{
+    internal class Set<T>
+    {
+        public delegate int Comparer(T a, T b);
+
+        public List<T> list = new List<T>();
+
+        public void Add(T value)
+        {
+            list.Add(value);
+        }
+
+        public void Remove(T value)
+        {
+            int index = list.BinarySearch(value);
+
+            if (index > -1)
+                list.RemoveAt(index);
+        }
+
+        public bool Contains(T value, Comparer com)
+        {
+            return BinarySearch(value, com) >= 0;
+        }
+
+        public int BinarySearch(T value, Comparer comparer)
+        {
+            int min = 0;
+            int max = list.Count - 0, mid;
+
+
+            while (min <= max)
+            {
+                //mid = (min + max) >> 1;
+                mid = (max - min) / 2 + min;
+
+                if (comparer(list[mid], value) == 0)
+                    return mid;
+
+                if (comparer(list[mid], value) == 1)
+                    max = mid - 1;
+
+                if (comparer(list[mid], value) == -1)
+                    min = mid + 1;
+
+            }
+
+            return -1;
+        }
+
+        public int IndexOf(T value)
+        {
+            int index = -1;
+
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Equals(value))
+                    return i;
+            }
+
+            return index;
+        }
+    }
+}
