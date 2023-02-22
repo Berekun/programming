ee897e07f0383a0c9c00994fabb2d64255d63cca
Berekun <xnatsuhd@gmail.com>
2023-02-22 20:47:13 +0100

    22/02/23

---
+++ FormularioWPF/FormularioWPF/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormularioWPF
+{
+    internal class Student
+    {
+        private string _name;
+        private string _description;
+        private int _age;
+
+        public string Name => _name;
+        public string Description => _description;
+        public int Age => _age;
+    }
+}
