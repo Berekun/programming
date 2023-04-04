d426d7b9917d8a01a99639631fe49fc0fa32838f
Berekun <xnatsuhd@gmail.com>
2023-04-04 20:00:02 +0200

    04/04/23

---
+++ Connections/Connections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace Connections
+{
+    internal class Program
+    {
+        public void Main()
+        {
+            try 
+            {
+                using (SqlConnection connection)
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
---
+++ TinyRpg/TinyRpg/Program.cs
@@ -0,0 +1,12 @@
+﻿using TinyRpgApp;
+
+namespace TinyRpg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UDK.Game.Launch(new RpgGame());
+        }
+    }
+}
