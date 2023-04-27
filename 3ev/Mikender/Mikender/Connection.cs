2b0948ca7d0c8099d3256f8981a36630b6997291
Berekun <xnatsuhd@gmail.com>
2023-04-27 18:53:04 +0200

    27/04/23

--- 3ev/Mikender/Mikender/Connection.cs
+++ 3ev/Mikender/Mikender/Connection.cs
@@ -19,31 +19,34 @@ namespace Mikender
                 using (SqlConnection connection = new SqlConnection("Data Source=192.168.56.101,1433;Initial Catalog=MIKENDER;User ID=sa;Password=SqlServer123;Connection Timeout=30"))
                 {
                     connection.Open();
-                    User user = new UserBuilder().SetName("hhhh").SetIdClient(7).Create();
+                    User user = new UserBuilder().SetIdClient(1).SetRating(14).Create();
                     string json = JsonSerializer.Serialize<User>(user);
-                    string sentence1 = "EXEC updateUser '" + json + "'";
-                    string sentence2 = "EXEC addUsuario '" + json + "'";
-                    string sentence3 = " SELECT * FROM dbo.GetFilteredUsers('" + json + "', 0, 10);";
-                    using (SqlCommand command = new SqlCommand(sentence3, connection))
+                    string sentence1 = "EXEC updateUser @value";
+                    string sentence2 = "EXEC addUsuario @value";
+                    string sentence3 = "EXEC RemoveUser @value";
+                    string sentence4 = " SELECT * FROM dbo.GetFilteredUsers(@value, 0, 10);";
+                    using (SqlCommand command = new SqlCommand(sentence1, connection))
                     {
+                        command.Parameters.AddWithValue("@value",json);
+
                         using (SqlDataReader reader = command.ExecuteReader())
                         {
                             while (reader.Read())
                             {
-                                int id = reader.GetFieldValue<int>(0);
-                                string name = reader.GetFieldValue<string>(1);
-                                int age = reader.GetFieldValue<int>(2);
-                                string image = reader.GetFieldValue<string>(3);
-                                string description = reader.GetFieldValue<string>(4);
-                                string gender = reader.GetFieldValue<string>(5);
-                                int rating = reader.GetFieldValue<int>(6);
-                                Console.WriteLine(id);
-                                Console.WriteLine(name);
-                                Console.WriteLine(age);
-                                Console.WriteLine(image);
-                                Console.WriteLine(description);
-                                Console.WriteLine(gender);
-                                Console.WriteLine(rating);
+                                //int id = reader.GetFieldValue<int>(0);
+                                //string name = reader.GetFieldValue<string>(1);
+                                //int age = reader.GetFieldValue<int>(2);
+                                //string image = reader.GetFieldValue<string>(3);
+                                //string description = reader.GetFieldValue<string>(4);
+                                //string gender = reader.GetFieldValue<string>(5);
+                                //int rating = reader.GetFieldValue<int>(6);
+                                //Console.WriteLine(id);
+                                //Console.WriteLine(name);
+                                //Console.WriteLine(age);
+                                //Console.WriteLine(image);
+                                //Console.WriteLine(description);
+                                //Console.WriteLine(gender);
+                                //Console.WriteLine(rating);
                             }
 
                         }
