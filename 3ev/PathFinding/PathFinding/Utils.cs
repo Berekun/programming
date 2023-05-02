a5f1b18b2f74a59138c029483359952f92cb29ed
Berekun <xnatsuhd@gmail.com>
2023-05-02 13:30:52 +0200

    02/05/23

---
+++ 3ev/PathFinding/PathFinding/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathFinding
+{
+    public class Utils
+    {
+        public static Random random = new Random();
+
+        public static int GetRandom(int min, int max)
+        {
+            if(min > max)
+                return random.Next(max, min);
+
+            return random.Next(min, max);
+        }
+    }
+}
